1ed1678390726aa22efd6eee2eb850c306074a80
Anj <subedianjan@yahoo.com.au>
2022-01-02 12:29:23 +1100

    comments

--- TwotyoeOf_forLoopo.py
+++ TwotyoeOf_forLoopo.py
@@ -7,7 +7,7 @@ S1 = input ( 'enter a string:')
 #S2 = ['none']*len(S1)
 S2 = []
 print (S1)
-
+# This loop uses index value directly.
 for indexValue in S1:
     print ( indexValue )
 
@@ -15,7 +15,7 @@ for indexValue in S1:
 
 # This for loop uses the index position to loop around. Here is pritns the string
 #last to first
-
+# Use of counter to avoid the array index out of bound error.
 counter1 = 1;
 for indexPostion in range(0, len(S1)):
     S2.append ( S1[len(S1) - counter1] )
